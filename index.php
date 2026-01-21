5d8fec8b5de0d5481aa9ebbf5f608b9e4adf0f97
unknown <Student@digitalschool.local>
2026-01-21 20:21:41 +0100

    new chaNGES

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php get_header(); ?>
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <h1>Blog</h1>
+            <div class="container">
+                <div class="blog-item">
+                    <?php
+                    if(have_posts()):
+                    while(have_posts()): the_post();
+                    get_template_part('parts/content');
+                    endwhile;
+                    ?>
+                    <div class="wpdevs-pagination">
+                        <div class="pages new">
+                            <?php previous posts_link("<< Newer posts"); ?>
+                        <div class="pages old">
+                            <?php previous_posts_link(">> Older posts"); ?>
+                        </div>
+                        </div>
+                        <?php
+                        else:
+                            ?>
+                        <p>Nothing yet to be displayed</p>
+                        <?php endif; ?>
+                    </div>
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+        </main>
+    </div>
+
+
+</div>
 <h1>This is our new theme</h1>
 <?php get_footer(); ?>
