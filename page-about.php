f2554a69e9bf84ff608d6a3f490f7770bab1f9ad
unknown <Student@digitalschool.local>
2026-01-26 20:25:45 +0100

    page aabout

---
+++ page-about.php
@@ -0,0 +1,52 @@
+<?php
+/*
+template name: about page
+*/
+?>
+<?php get_header(); ?>
+<div id="content" clas="site-main">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">\
+                <h1>About us</h1>
+                <p>welcome to our tech corner- where innovation meets explanation</p>
+
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who we are</h2>
+                <p>We are a team of developers inginieers and tech enthusiast who love to write about technology</p>
+
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2>Our mission</h2>
+                <p>Our goal is simple: to empower curious minds and passionate techies by decliers accurate, useful and accessible content</p>
+
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What we cover</h2>
+            <ul>
+                <li>Programming tutorial and tips</li>
+                <li>Software and app reviews</li>
+                <li>Web development, Devops and cloud computing</li>
+                <li>Trends in tech and industry news</li>
+            </ul>
+            </div>
+        </section>
+
+        <section class="about-contant">
+            <div class="container">
+                <h2>Want to Connect?</h2>
+                <p>We love hearing from our readers! Reach out to us via our contact page or follow us on social media to stay updated with the latest posts and tech news</p>
+            </div>
+    </main>
+</div>
+<?php get_footer(); ?>
